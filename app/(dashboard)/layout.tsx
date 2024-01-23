f8f4d8f5636191cd2cc7f64d6caf4f55fac0fc5c
Noodle96 <jorgealfredomarino2@gmail.com>
2024-01-23 16:38:06 -0500

    Agregar Sidebar al layout de (dashboard) y agregar el logo y el name

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,12 +1,11 @@
 import NavBar from "@/components/NavBar";
+import SideBar from "@/components/SideBar";
 
 const DashboardLayout = ({children}:{children: React.ReactNode}) => {
 	return (
 		<div className="h-full relative">
 			<div className=" hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
-				<div>
-					Hello sidebar
-				</div>
+				<SideBar />
 			</div>
 			<main className="md:pl-72 bg-red-400">
 				{/* insertar navbar component here */}

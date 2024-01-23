29695020b0e6def2ae9b30b71e33b5731f918ae6
Noodle96 <jorgealfredomarino2@gmail.com>
2024-01-23 10:52:21 -0500

    Add userButton and redirect to "/" when sign-out

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 const DashBoardPage = () => {
 	return (
 		<div>
 			<h1>Dashboard Page(protected)</h1>
+			<UserButton afterSignOutUrl="/"/>
 		</div>
 	);
 }

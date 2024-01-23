20af65718b8a5ce95fcd5c5078b1639b9a06e315
Noodle96 <jorgealfredomarino2@gmail.com>
2024-01-23 10:46:41 -0500

    Configurando the landing Page to login and register

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,8 +1,19 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 // sfc
 const LandingPage = () => {
 	return ( 
 		<div>
 			<h1>Landing Page(unprotected)</h1>
+			<div>
+				<Link href='sign-in'>
+					<Button>Login</Button>
+				</Link>
+				<Link href='sign-up'>
+					<Button>Registrarse</Button>
+				</Link>
+			</div>
 		</div>
 	);	
 }

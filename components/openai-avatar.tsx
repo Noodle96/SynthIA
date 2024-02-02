46aacf7f694517a296b7b3d67a2a29b34ddfd29d
Noodle96 <jorgealfredomarino2@gmail.com>
2024-02-02 09:06:13 -0500

    Crear el componente OpenAIAvatar

---
+++ components/openai-avatar.tsx
@@ -0,0 +1,9 @@
+import { Avatar, AvatarImage } from "./ui/avatar";
+
+export const OpenAIAvatar = () => {
+	return (
+		<Avatar className="h-8 w-8">
+			<AvatarImage src="/logo.png" />
+		</Avatar>
+	);
+}
